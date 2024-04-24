0d172168c073469a026c91ec4378716bfedcb9e8
camilo andres soler bucuru <camilo_soler@localhost.localdomain>
2024-04-24 08:41:57 -0500

    statics

--- market/settings.py
+++ market/settings.py
@@ -128,6 +128,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [BASE_DIR, 'app/static']
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
