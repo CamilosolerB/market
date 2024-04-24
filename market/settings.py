986e32008573ec384cda34c6c0c157a12595c259
camilo andres soler bucuru <camilo_soler@localhost.localdomain>
2024-04-24 08:42:08 -0500

    statics

--- market/settings.py
+++ market/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
 STATICFILES_DIRS = [BASE_DIR, 'app/static']
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
