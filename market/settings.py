e3c68af9568042f289b4393d865a4dac37ae84a5
Camilo Soler <89280685+CamilosolerB@users.noreply.github.com>
2024-04-28 13:43:18 -0500

    Update settings.py

--- market/settings.py
+++ market/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
 STATICFILES_DIRS = [BASE_DIR, 'app/static']
 #if not DEBUG:
 #    STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
