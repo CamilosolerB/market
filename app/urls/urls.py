16d1334c84a048be548805e03541880e5d3442bf
MeryT9 <meryissatorres@gmail.com>
2025-10-27 20:13:33 -0500

    Cambios Mery

--- app/urls/urls.py
+++ app/urls/urls.py
@@ -1,8 +1,10 @@
+# app/urls/urls.py
 from django.urls import path
 from ..views import views
 
 urlpatterns = [
-    path('',views.home_page, name="index"),
-    path('login/',views.login_validation, name="login"),
-    path('singout/',views.cerrar_sesion, name="singout")
+    path('', views.home_page, name='index'),
+    path('login/', views.login_validation, name='login'),
+    path('registro/', views.registro_usuario, name='registro'),
+    path('logout/', views.cerrar_sesion, name='logout'),
 ]
